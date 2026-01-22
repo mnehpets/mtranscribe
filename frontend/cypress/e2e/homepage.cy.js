757d27d88bae2d9eafe9954ba6e37859128b5d12
Stephen Ma <mnehpets@gmail.com>
2026-01-22 01:46:45 +0000

    Fix e2e frontend tests.

--- frontend/cypress/e2e/homepage.cy.js
+++ frontend/cypress/e2e/homepage.cy.js
@@ -1,11 +1,6 @@
 describe('Homepage', () => {
   it('displays the application title', () => {
-    cy.visit('/')
+    cy.visit('/u/')
     cy.contains('mtranscribe').should('be.visible')
-  })
-
-  it('displays the description', () => {
-    cy.visit('/')
-    cy.contains('Live transcription web application').should('be.visible')
   })
 })
