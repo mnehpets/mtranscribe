27079d0ac1ba83646dcb94ab367e6b989a0855b7
Stephen Ma <mnehpets@gmail.com>
2026-02-05 06:37:22 +0000

    vite dev proxy to backend.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -10,7 +10,17 @@ export default defineConfig({
     Icons({ compiler: 'vue3' })
   ],
   server: {
-    port: 3000
+    port: 3000,
+    proxy: {
+      '/auth': {
+        target: 'http://localhost:8080',
+        changeOrigin: true
+      },
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true
+      }
+    }
   },
   test: {
     globals: true,
